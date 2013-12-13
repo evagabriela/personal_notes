2efa45840673032265c93cf7c470ea13534a2276
Eva Zamudio <gabriela.osu@gmail.com>
2013-12-13 12:43:06 -0800

    add tutorial javascript notes

---
+++ javascriptNotes.js
@@ -0,0 +1,41 @@
+var num = 1
+
+while (num<=10) {
+  console.log(num);
+  num++;
+}
+
+// for (*start with this* ; *loop if this expression is true* ; *do this after each loop*){
+//   in each loop, do this code!*
+// }
+
+for ( var trainNumber = 1; traiNumber <= trainsOperational; trainNumber++){
+  console.log("Train#" + trainNumber + "is running");
+}
+
+for ( var number = 5; number >0; number--){
+  console.log(number
+
+    )
+}
+
+// Which trains are not running? 
+var totalTrains = 12;
+var trainOperational = 8; 
+
+for (var stoppedTrain = trainsOperational +1; stoppedTrain <= totalTrains; stoppedTrain++{
+  console.log("Train#" + stoppedTrain + " is not operational")
+})
+
+// With while loop 
+var totalTrains = 12;
+var trainsOperational = 8;
+
+var trainNumber =1;
+while(trainNumber <= trainsOperational){
+  console.log("Train#" + trainNumber + "is running");
+  trainNumber++;
+}
+
+
+
